8ed7b713863a61e0eb13df82747343c0f513e41a
Nathan Lemoine <nl024482@chbah.org>
2024-02-14 10:03:02 +0100

    Add more exercises

--- german-sysadmin/lib/username.ex
+++
@@ -1,9 +0,0 @@
-defmodule Username do
-  def sanitize(username) do
-    # ä becomes ae
-    # ö becomes oe
-    # ü becomes ue
-    # ß becomes ss
-    
-  end
-end
--- name-badge/mix.exs
+++ name-badge/mix.exs
@@ -1,9 +1,9 @@
-defmodule Username.MixProject do
+defmodule NameBadge.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :german_sysadmin,
+      app: :name_badge,
       version: "0.1.0",
       # elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
