1a45f699b484ab6032da6c49209d096da2bf76b1
Nathan Lemoine <nl024482@chbah.org>
2024-03-20 14:47:14 +0100

    Refactor compare function in GuessingGame module

--- guessing-game/lib/guessing_game.ex
+++ guessing-game/lib/guessing_game.ex
@@ -1,18 +1,8 @@
 defmodule GuessingGame do
   def compare(secret_number, guess \\ :no_guess)
-  def compare(_, :no_guess) do
-    "Make a guess"
-  end
-  def compare(secret_number, guess) when secret_number === guess do
-    "Correct"
-  end
-  def compare(secret_number, guess) when ((secret_number - 1) === guess) or ((secret_number + 1) === guess) do
-    "So close"
-  end
-  def compare(secret_number, guess) when secret_number > guess do
-    "Too low"
-  end
-  def compare(secret_number, guess) when secret_number < guess do
-    "Too high"
-  end
+  def compare(_, :no_guess), do: "Make a guess"
+  def compare(secret_number, guess) when secret_number === guess, do: "Correct"
+  def compare(secret_number, guess) when abs(secret_number - guess) == 1, do: "So close"
+  def compare(secret_number, guess) when secret_number > guess, do: "Too low"
+  def compare(secret_number, guess) when secret_number < guess, do: "Too high"
 end
