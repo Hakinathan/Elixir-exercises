26e5b7986360bc0766c493fcfb60dd1b82a9f639
Nathan Lemoine <nl024482@chbah.org>
2024-04-08 11:03:49 +0200

    Refactor TakeANumber module to improve code organization and readability

--- take-a-number/lib/take_a_number.ex
+++ take-a-number/lib/take_a_number.ex
@@ -1,5 +1,25 @@
 defmodule TakeANumber do
   def start() do
-    spawn(fn -> 0 end)
+    initial_state = 0
+    spawn(fn -> loop(initial_state) end)
+  end
+
+  defp loop(state) do
+    receive do
+      {:take_a_number, sender_pid} ->
+        new_state = state + 1
+        send(sender_pid, new_state)
+        loop(new_state)
+
+      {:report_state, sender_pid} ->
+        send(sender_pid, state)
+        loop(state)
+
+      :stop ->
+        Process.exit(self(), :normal)
+
+      _ ->
+        loop(state)
+    end
   end
 end
